a8fa3083f5ed3a0bbf42cc5866d98e7a04f01f60
7220 <kumasaki@naver.com>
2023-04-16 00:39:18 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Subject from "./component/subject";
+import TOC from "./component/TOC";
+import Content from "./component/content";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "welcome",
+      subject: { title: "WEB", sub: "World wide web!" },
+      welcome: { title: "Welcome", desc: "Hello, React" },
+      contents: [
+        { id: 1, title: "HTML", decs: "HTML is ..." },
+        { id: 2, title: "CSS", decs: "CSS is ..." },
+        { id: 3, title: "JavaScript", decs: "JavaScript is ..." },
+      ],
+    };
+  }
+  render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.subject.title;
+      _desc = this.state.subject.desc;
+    }
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
